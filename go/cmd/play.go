d28f9823f41c4ba935ddc7c38816b55abd2247d3
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2021-04-03 17:21:59 +0100

    Remove redundant Go libs + update godo scripts

--- go/cmd/play.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func main() {
-	// Use this main to play around with the cookies
-	// Run by typing './godo run' from root directory
-}
---
+++ go/playground/playground.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// Write play code here...
+
+	fmt.Println()
+	fmt.Println("All work and no play makes Jack a dull boy,")
+	fmt.Println("All play and no work makes Jack a mere toy.")
+
+}
