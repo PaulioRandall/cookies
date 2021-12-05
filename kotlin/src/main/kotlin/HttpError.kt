322102c8d64d71ae84e3639c19d79a4cf2964686
Paul Williams <29413800+PaulioRandall@users.noreply.github.com>
2021-12-05 12:08:58 +0000

    Reduce comment verbosity in HttpError.kt

--- kotlin/src/main/kotlin/HttpError.kt
+++ kotlin/src/main/kotlin/HttpError.kt
@@ -1,11 +1,5 @@
 
-/******************************************************************************
- * Thrown when a HTTP service encounters an error
- *
- * @property[status] HTTP status code
- * @property[publicMsg] Error message
- * @param[cause] Cause of the error
- *****************************************************************************/
+// Thrown when a HTTP service encounters an error.
 open class HttpError(
   val status: Int,
   val publicMsg: String,
@@ -23,46 +17,25 @@ open class HttpError(
 
   companion object {
 
-    /**************************************************************************
-     * Returns a client error with a 400 status when a client supplied
-     * parameter is missing or invalid
-     *
-     * @param[publicMsg] Public response message
-     *************************************************************************/
+    // Returns a client error with a 400 status when a client supplied
+    // parameter is missing or invalid.
     fun badRequest(publicMsg: String) = HttpError(400, publicMsg)
 
-    /**************************************************************************
-     * Returns a client error with a 401 status when a client attempts to
-     * access a resource before they have authenticated or there authentication
-     * details were invalid
-     *
-     * @param[publicMsg] Public response message
-     *************************************************************************/
+    // Returns a client error with a 401 status when a client attempts to
+    // access a resource before they have authenticated or there authentication
+    // details were invalid.
     fun badLogin(publicMsg: String) = HttpError(401, publicMsg)
 
-    /**************************************************************************
-     * Returns a client error with a 403 status when a client attempts to
-     * access a resource they are not authorised to access
-     *
-     * @param[publicMsg] Public response message
-     *************************************************************************/
+    // Returns a client error with a 403 status when a client attempts to
+    // access a resource they are not authorised to access.
     fun notAllowed(publicMsg: String) = HttpError(403, publicMsg)
 
-    /**************************************************************************
-     * Returns a client error with a 404 'not found' status
-     *
-     * @param[publicMsg] Public response message
-     *************************************************************************/
+    // Returns a client error with a 404 'not found' status.
     fun notFound(
       publicMsg: String = "No such resource"
     ) = HttpError(404, publicMsg)
 
-    /**************************************************************************
-     * Returns a service error with a 500 status when a bug is detected
-     *
-     * @param[internalMsg] Message for internal use, logs etc..
-     * @param[cause] Cause of the error
-     *************************************************************************/
+    // Returns a service error with a 500 status when a bug is detected.
     fun bug(
       internalMsg: String,
       cause: Exception? = null
@@ -72,37 +45,22 @@ open class HttpError(
       Exception(internalMsg, cause)
     )
 
-    /**************************************************************************
-     * Returns a service error with a 500 status when a bad service
-     * configuration is detected
-     *
-     * @param[publicMsg] Public response message
-     * @param[cause] Cause of the error
-     *************************************************************************/
+    // Returns a service error with a 500 status when a bad service
+    // configuration is detected.
     fun badConfig(
       publicMsg: String,
       cause: Exception? = null
     ) = HttpError(500, publicMsg, cause)
 
-    /**************************************************************************
-     * Returns a service error with a 501 status when a service feature has
-     * been declared but not implemented
-     *
-     * @param[publicMsg] Public response message
-     * @param[cause] Cause of the error
-     *************************************************************************/
+    // Returns a service error with a 501 status when a service feature has
+    // been declared but not implemented.
     fun notImplemented(
       publicMsg: String,
       cause: Exception? = null
     ) = HttpError(501, publicMsg, cause)
 
-    /**************************************************************************
-     * Returns a service error with a 503 status when a service feature is
-     * unavailable due to a known issue or maintenance
-     *
-     * @param[publicMsg] Public response message
-     * @param[cause] Cause of the error
-     *************************************************************************/
+    // Returns a service error with a 503 status when a service feature is
+    // unavailable due to a known issue or maintenance.
     fun featureUnavailable(
       publicMsg: String,
       cause: Exception? = null
